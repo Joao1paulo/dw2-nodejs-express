d364f72cb00a3ff1d47e44cf5924434a7a28d355
DSM2 <fatec-dsm2@academico.intra>
2025-02-13 19:28:52 -0300

    Teste de commit

---
+++ js-aula-0.1.js
@@ -0,0 +1,22 @@
+//Comentário em JS
+
+/*Comentário em linha
+em vbloco
+*/
+
+//Seleciona e Ctrl + , 
+
+//Declaração de variáveis no JS
+// CONST X LET X VAR
+
+const nome = "joão" // Uma const precisa de um valor inicial
+let Nome // Permite iniciar a variável vazia
+// const nome = "João" -> Resulta em um erro
+// let nNome
+var cor = "Azul"
+var cor = "Amarelo"
+let cidade = "Registro"
+cidade = "Jacupiranga" // Let permite alterar o valor.
+
+const message = "Hello, Word! Iniciando estudos com Javascript!"
+console.log(message)
