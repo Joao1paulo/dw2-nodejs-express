28409e674b11c630db79aaf666889d54ae1e8829
João Paulo Goreri Rustichelli <99690766+Joao1paulo@users.noreply.github.com>
2025-03-14 10:21:32 -0300

    atv2.js

--- aula-0.2-arrays-e-objetos/atv2.js
+++ aula-0.2-arrays-e-objetos/atv2.js
@@ -3,3 +3,49 @@ const estados = [ ' Acre', ' Alagoas', ' Amazonas', ' Bahia', ' Ceará', ' Espí
 estados.forEach((produtos, i) => {
     console.log(`${i - 1} - ${produtos}`);
 })
+
+//OBJETOS LITERAIS
+const jp = [
+    {
+        nome: "João Paulo",
+        idade: 19,
+        cidade: "Registro",
+        hobby: "Tenis de mesa",
+        musica: "Can't stop - Earth Wind & Fire"
+},
+]
+
+jp.forEach((jp) => { //
+    console.log(`\n Nome: ${jp.nome} \n Idade: ${jp.idade} \n Cidade: ${jp.cidade} \n hobby: ${jp.hobby} \n Música preferida: ${jp.musica}`)
+    }
+ )
+
+
+//ARRAY DE OBJETOS
+const filmes = [
+    {
+        titulo: "O Poderoso Chefão",
+        genero: "Drama",
+        ano: "1972", 
+        classificacao: "16 anos"
+    }, {
+        titulo: "A Lista de Schindler",
+        genero: "Drama",
+        ano: "1993", 
+        classificacao: "14 anos"
+    }, {
+        titulo: "O Senhor dos Anéis: O Retorno do Rei",
+        genero: "Aventura",
+        ano: "2003",
+        classificacao: "12 anos"
+    }, {
+        titulo: "O Rei Leão",
+        genero: "Animação",
+        ano: "1994",
+        classificacao: "Livre"
+    }
+]
+
+filmes.forEach ( (filmes) => {
+    console.log(`\n Título: ${filmes.titulo} \n Gênero: ${filmes.genero} \n Ano: ${filmes.ano} \n Classificação: ${filmes.classificacao}`)
+})
